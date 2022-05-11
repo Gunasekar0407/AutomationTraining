827f1d497e1ca6a3323ee31ed2c364cb9da33e04
Gunasekar D <88372178+Gunasekar0407@users.noreply.github.com>
2022-05-11 20:00:57 +0530

    Add files via upload

---
+++ Abstract/Athelete.java
@@ -0,0 +1,15 @@
+package Abstract;
+
+abstract class Athelete {
+	public void eat() {
+		System.out.println("I eat healthy food.");
+	}
+
+	public void sleep() {
+		System.out.println("I sleep 8hours a day");
+	}
+
+	abstract void event();
+
+	abstract void accomplishment();
+}
